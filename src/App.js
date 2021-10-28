daf412a95af817978367fc52a0b64d2c229fa35a
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-10-28 16:18:00 -0500

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/headerComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
