509a04e4d06672f395ee5f9a3eb95b0c9b929df2
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-10-28 16:43:42 -0500

    Added some styles, worked on homeComponent

---
+++ src/components/homeComponent.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+function Home() { 
+    return (
+        <div className="container-fluid home">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 pt-4 pt-sm-4">
+                        <h1>Hi, I am Prakash!</h1>
+                        
+                        <h4 id="homeH4">I create beautiful and responsive websites</h4><br></br>
+
+                        <p className="homeParagraph">I am a front-end developer based in Dallas, Texas. I’m passionate about learning the latest and greatest technologies whether that’s the front-end or back-end. I enjoy creating sleek, responsive and user friendly webpages. I am currently open to opportunities and if you feel like we would be a great match, I’d love to hear from you!</p><br />
+                        <div className="mobileCenter"><Link to={ `/projects` }><Button className="btn btn-ghost btn-lg mr-3 mobileCenter">Projects</Button></Link>
+                        <Link to={ `/contact` }><Button className="btn btn-ghost btn-lg mobileCenter">Get In Touch</Button></Link></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,14 +1,14 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Header from "./headerComponent";
+import Home from "./homeComponent";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
-            <Header />
+            <React.Fragment>
+                <Header />
+                <Home />
+            </React.Fragment>
         )
     }
 }
