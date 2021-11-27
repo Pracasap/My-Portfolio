d217d2c2b18d1ec05378c441c2290a83bd64e25b
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-11-26 23:58:17 -0600

    changed to forever-avenue

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,10 +1,10 @@
 export const PROJECTS = [
     {
         "id": 10,
-        "name": "Forever Creating",
+        "name": "Forever Avenue",
         "image": "images/foreverCreating.png",
-        "website": "https://forevercreating.netlify.app/",
-        "github": "https://github.com/Pracasap/Forever-Creating",
+        "website": "https://foreveravenue.netlify.app/",
+        "github": "https://github.com/Pracasap/Forever-Avenue",
         "codepen": false,
         "description": "This website allows a creative person sell products online. It has a shop which displays variety of hand-made creative items like earrings, oils, bath salts, etc. I learned react hooks, learned to have video background on a webpage, etc. 🙌"
     },
